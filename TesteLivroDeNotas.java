ab701ad1a71af2a6dab7f03070b54da93da1fbc7
João Pedro Martins Forte <joao.forte@fatec.sp.gov.br>
2023-09-17 18:54:16 -0300

    ap06(java): teste do livro de notas

---
+++ TesteLivroDeNotas.java
@@ -0,0 +1,6 @@
+public class TesteLivroDeNotas {
+    public static void main(String[] args){
+        LivroDeNotas livroDeNotas = new LivroDeNotas();
+        livroDeNotas.exibirMensagem();
+    }
+}
