2bb7dd6820f886b697478872468e255c53e07090
João Pedro Martins Forte <joao.forte@fatec.sp.gov.br>
2023-09-17 20:45:34 -0300

    ap06(java): fim da listagem 12.1

--- LivroDeNotas.java
+++ LivroDeNotas.java
@@ -1,5 +1,32 @@
 public class LivroDeNotas {
-    public static void exibirMensagem(){
-        System.out.println("Bem-vindo ao livro de notas.");
+
+    // Variável de instância
+    // Instância é sinônimo de objeto
+    // Modificador de acesso
+    // Encapsulamento
+
+    private String nomeDoCurso;
+
+    public void exibirMensagem(){
+        System.out.printf("Bem-vindo ao livro de notas do curso %s", getNomeDoCurso());
+    }
+
+    // Métodos Getters/Setters
+
+    // Getter
+    public String getNomeDoCurso() {
+        return nomeDoCurso;
+    }
+
+    // Setter
+    public void setNomeDoCurso(String nomeDoCurso) {
+        //shadowing
+        this.nomeDoCurso = nomeDoCurso;
+    }
+
+    // Construtor padrão
+    LivroDeNotas(String nomeDoCurso){
+        // Shadowing
+        this.nomeDoCurso = nomeDoCurso;
     }
 }
--- TesteLivroDeNotas.java
+++ TesteLivroDeNotas.java
@@ -1,6 +1,9 @@
 public class TesteLivroDeNotas {
     public static void main(String[] args){
-        LivroDeNotas livroDeNotas = new LivroDeNotas();
+        String nomeDoCurso = javax.swing.JOptionPane.showInputDialog("Prof, qual o nome do curso?");
+        LivroDeNotas livroDeNotas = new LivroDeNotas(nomeDoCurso);
+        livroDeNotas.setNomeDoCurso(nomeDoCurso);
         livroDeNotas.exibirMensagem();
+        System.out.println("\nA disciplina e " + livroDeNotas.getNomeDoCurso());
     }
 }
